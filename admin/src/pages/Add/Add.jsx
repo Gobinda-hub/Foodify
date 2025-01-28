136cce5046c30611b318a27bad9fa8cea968ba35
Gobinda Mandal <mgobinda204@gmail.com>
2025-01-28 18:10:01 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -25,7 +25,7 @@ const Add = () => {
         formData.append("price", Number(data.price));
         formData.append("category", data.category);
         formData.append("image", image);
-        const response = await axios.post(`₹{url}/api/food/add`, formData);
+        const response = await axios.post(`${url}/api/food/add`, formData);
         if (response.data.success) {
             toast.success(response.data.message)
             setData({
@@ -79,7 +79,7 @@ const Add = () => {
                     </div>
                     <div className='add-price flex-col'>
                         <p>Product Price</p>
-                        <input type="Number" name='price' onChange={onChangeHandler} value={data.price} placeholder='₹25' />
+                        <input type="Number" name='price' onChange={onChangeHandler} value={data.price} placeholder='$25' />
                     </div>
                 </div>
                 <button type='submit' className='add-btn' >ADD</button>
