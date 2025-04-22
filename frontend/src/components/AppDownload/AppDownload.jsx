24b151cc86a6ae0375f60027bb33a18311a7f5b5
Gobinda Mandal <mgobinda204@gmail.com>
2025-04-22 21:06:34 +0530

    Update AppDownload.jsx

--- frontend/src/components/AppDownload/AppDownload.jsx
+++ frontend/src/components/AppDownload/AppDownload.jsx
@@ -7,8 +7,14 @@ const AppDownload = () => {
         <div className='app-download' id='app-download'>
             <p>For Better Experience Download <br />Foodify App</p>
             <div className="app-download-platforms">
-                <img src={assets.play_store} alt="" />
-                <img src={assets.app_store} alt="" />
+               <a href="https://play.google.com/store/apps/details?id=com.foodify24x7.app">
+                    <img src={assets.play_store} alt="" />
+                </a>
+                <a href="https://play.google.com/store/apps/details?id=com.foodify24x7.app">
+                    <img src={assets.app_store} alt="" />
+                </a>
+                {/* <img src={assets.play_store} alt="" />
+                <img src={assets.app_store} alt="" /> */}
             </div>
         </div>
     )
