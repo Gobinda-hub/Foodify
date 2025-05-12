0660622cdce8a619a3027ca406284ba20f5a7bad
Gobinda Mandal <mgobinda204@gmail.com>
2025-05-12 20:18:03 +0530

    Update PlaceOrder.jsx

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -96,9 +96,9 @@ const PlaceOrder = () => {
                     <div>
                         <div className="cart-total-details"><p>Subtotal</p><p>₹ {getTotalCartAmount()}</p></div>
                         <hr />
-                        <div className="cart-total-details"><p>Delivery Fee</p><p>₹ {getTotalCartAmount() === 0 ? 0 : 5}</p></div>
+                        <div className="cart-total-details"><p>Delivery Fee</p><p>₹ {getTotalCartAmount() === 0 ? 0 : 50}</p></div>
                         <hr />
-                        <div className="cart-total-details"><b>Total</b><b>₹ {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 5}</b></div>
+                        <div className="cart-total-details"><b>Total</b><b>₹ {getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 50}</b></div>
                     </div>
                 </div>
                 <button className='place-order-submit' type='submit'>Proceed To Payment</button>
